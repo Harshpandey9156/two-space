d339c013e9f1d563a62c23be3b6ab15c8fe8de73
somnath-choudhury <choudhurysomnath2000@gmail.com>
2025-10-20 19:59:53 +0530

    only profile posts page done

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -58,6 +58,8 @@ export default function Discover() {
     }
 
     fetchPosts()
+          console.log();
+
   }, [firebaseUser])
 
   if (loading || !firebaseUser){
@@ -71,7 +73,7 @@ export default function Discover() {
       <h1 className="text-2xl font-semibold mb-4">Discover</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {posts.length === 0 && <p>No posts found.</p>}
+        {posts.length === 0 && <p>No posts found.</p>}        
 
         {posts.map((p) => (
           <div key={p.id} className="bg-white shadow rounded-lg p-4">
---
+++ src/app/our-space/page.tsx
@@ -0,0 +1,90 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "@/lib/firebase";
+
+
+interface Post {
+  id: string;
+  authorId: string;
+  content: string;
+  imageUrl: string;
+  createdAt: any;
+  isPublic: boolean;
+  authorName?: string;
+  authorPhoto?: string;
+}
+
+export default function OurSpace() {
+    const {firebaseUser, loading} = useAuth();
+    const [posts, setPosts] = useState<Post[]>([]);
+    const[loadingPosts, setLoadingPosts] = useState(true)
+
+    useEffect(() => {
+        if(!loading && firebaseUser) {
+            const fetchMyPosts = async () => {
+                try {
+                    const postsRef = collection(db, 'posts')
+                    const q = query(postsRef, where("authorId", '==', firebaseUser.uid))
+                    const querySnapshot = await getDocs(q);
+
+                    const myPosts: Post[] = querySnapshot.docs.map((doc) => ({
+                        id: doc.id,
+                        ...doc.data(),
+                        })) as Post[];
+
+                    setPosts(myPosts)
+                }
+                catch (error) {
+                    console.error(error);
+                }
+                finally {
+                    setLoadingPosts(false)
+                }
+            }
+            fetchMyPosts()
+        }
+        
+    }, [firebaseUser, loading])
+
+    if (loading || loadingPosts) {
+        return (
+            <p>Loading post...</p>
+        )
+    }
+
+    if (!firebaseUser) {
+        return (
+            <p>You must be logged in to view posts</p>
+        )
+    }
+
+    if (posts.length === 0) {
+        return (
+            <p>No posts yet. Create your first post.</p>
+        )
+    }
+
+    return (
+        <div className="space-y-4">
+      <h2 className="text-xl font-semibold mb-2">My Posts</h2>
+      {posts.map((post) => (
+        <div key={post.id} className="border rounded-lg p-4 shadow-sm">
+          {post.imageUrl && (
+            <img
+              src={post.imageUrl}
+              alt="Post"
+              className="rounded-lg mb-2 max-h-64 w-full object-cover"
+            />
+          )}
+          <p>{post.content}</p>
+          <p className="text-sm text-gray-500 mt-1">
+            {new Date(post.createdAt?.seconds * 1000).toLocaleString()}
+          </p>
+        </div>
+      ))}
+    </div>
+    )
+}
